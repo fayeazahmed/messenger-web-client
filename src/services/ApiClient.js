b5cde6a51721b1a435b260ae0d6b979aa3e32b4e
Fayeaz Ahmed <r10.ahmed@yahoo.com>
2025-05-26 00:17:45 +0600

    deployment fixes

--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -4,7 +4,7 @@ class ApiClient {
     constructor(baseURL) {
         this.client = axios.create({
             baseURL: baseURL,
-            timeout: 10000,
+            timeout: 50000,
             headers: {
                 'Content-Type': 'application/json',
             },
